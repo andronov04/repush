778f42ae534d9b0cc585b0fcb24da1a25613a770
Andrey Andronov <me@andronov.io>
2019-08-07 19:42:42 +0300

    Add slidable chat and delete

--- lib/src/ui/widgets/chat_item.dart
+++ lib/src/ui/widgets/chat_item.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:repush/src/blocs/chat_bloc_provider.dart';
 import 'package:repush/src/models/chat.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import '../../../utils.dart';
 
 class ChatItemScreen extends StatefulWidget {
@@ -40,6 +41,51 @@ class _ChatItemState extends State<ChatItemScreen> {
     var chatActiveIndex = -1;
     var item = widget.chat;
 
+//    return Slidable(
+//      actionPane: SlidableDrawerActionPane(),
+//      actionExtentRatio: 0.25,
+//      child: Container(
+//        color: Colors.white,
+//        child: ListTile(
+//          leading: CircleAvatar(
+//            backgroundColor: Colors.indigoAccent,
+//            child: Text('ss'),
+//            foregroundColor: Colors.white,
+//          ),
+//          title: Text('Tile n°'),
+//          subtitle: Text('SlidableDrawerDelegate'),
+//        ),
+//      ),
+//      actions: <Widget>[
+//        IconSlideAction(
+//          caption: 'Archive',
+//          color: Colors.blue,
+//          icon: Icons.archive,
+////          onTap: () => _showSnackBar('Archive'),
+//        ),
+//        IconSlideAction(
+//          caption: 'Share',
+//          color: Colors.indigo,
+//          icon: Icons.share,
+////          onTap: () => _showSnackBar('Share'),
+//        ),
+//      ],
+//      secondaryActions: <Widget>[
+//        IconSlideAction(
+//          caption: 'More',
+//          color: Colors.black45,
+//          icon: Icons.more_horiz,
+////          onTap: () => _showSnackBar('More'),
+//        ),
+//        IconSlideAction(
+//          caption: 'Delete',
+//          color: Colors.red,
+//          icon: Icons.delete,
+////          onTap: () => _showSnackBar('Delete'),
+//        ),
+//      ],
+//    );
+
     if(item.isPending){
       print(item.to.id);
       print(widget._currentUserUid);
@@ -49,97 +95,109 @@ class _ChatItemState extends State<ChatItemScreen> {
       print(to);
 
       return new Container(
-        margin: EdgeInsets.only(
-            left: 20.0,
-            right: 20.0,
-            top: 20.0
-        ),
-        child: new Container(
-          width: MediaQuery.of(context).size.width,
-          height: 40.0,
+        child: Slidable(
+          actionPane: SlidableDrawerActionPane(),
+          actionExtentRatio: 0.25,
+//          secondaryActions: <Widget>[
+//            IconSlideAction(
+//              caption: 'Delete',
+//              icon: Icons.delete,
+//            ),
+//          ],
+          child: new Container(
+            margin: EdgeInsets.only(
+                left: 20.0,
+                right: 20.0,
+                top: 20.0
+            ),
+            child: new Container(
+              width: MediaQuery.of(context).size.width,
+              height: 40.0,
 //        height: MediaQuery.of(context).size.height,
 
-          child: new Material(
+              child: new Material(
 //          borderRadius: BorderRadius.circular(100.0),
-            child: new Container(
                 child: new Container(
-                  padding: EdgeInsets.only(
-                    left: 12.0,
-                    right: 4.0,
-                    top: 4.0,
-                    bottom: 4.0
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                    child: new Container(
+                      padding: EdgeInsets.only(
+                          left: 12.0,
+                          right: 4.0,
+                          top: 4.0,
+                          bottom: 4.0
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
                         children: <Widget>[
-                          new Text(to ? 'REQUESTED' : 'REQUEST',
-                              style: TextStyle(
-                                  color: Colors.black54,
-                                  fontSize: 10.0
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              new Text(to ? 'REQUESTED' : 'REQUEST',
+                                  style: TextStyle(
+                                      color: Colors.black54,
+                                      fontSize: 10.0
+                                  )
+                              ),
+                              Text(item.userNameChat(widget._currentUserUid),
+                                  style: TextStyle(
+                                      color: Colors.black54,
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.bold
+                                  )
                               )
+                            ],
                           ),
-                          Text(item.userNameChat(widget._currentUserUid),
-                              style: TextStyle(
-                                  color: Colors.black54,
-                                  fontSize: 16.0,
-                                fontWeight: FontWeight.bold
-                              )
+                          new Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              !to ? IconButton(
+                                icon: Icon(Icons.clear, color: Colors.black54, size: 16.0),
+                                padding: EdgeInsets.all(0.0),
+                                focusColor: Colors.transparent,
+                                hoverColor: Colors.transparent,
+                                highlightColor: Colors.transparent,
+                                splashColor: Colors.transparent,
+                                iconSize: 2.0,
+                                tooltip: 'Increase volume by 10',
+                                onPressed: () {
+//                              setState(() {
+//                              });
+                                  _bloc.confirmChat(item.id, true);
+                                },
+                              ) : Text(''),
+                              FlatButton(
+                                child: Text(
+                                  to ? 'CANCEL' : 'CONFIRM',
+                                  style: TextStyle(
+                                    fontSize: 16.0,
+                                    fontFamily: 'Arial',
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                                textColor: Colors.white,
+                                color: Colors.black54,
+                                shape:
+                                RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+                                onPressed: () {
+                                  if(to){
+                                    _bloc.confirmChat(item.id, true);
+                                  }
+                                  else{
+                                    _bloc.confirmChat(item.id, false);
+                                  }
+//                      this.createMessage();
+                                  controller.text = '';
+                                },
+                              ),
+                            ],
                           )
                         ],
                       ),
-                      new Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          !to ? IconButton(
-                            icon: Icon(Icons.clear, color: Colors.black54, size: 16.0),
-                            padding: EdgeInsets.all(0.0),
-                            focusColor: Colors.transparent,
-                            hoverColor: Colors.transparent,
-                            highlightColor: Colors.transparent,
-                            splashColor: Colors.transparent,
-                            iconSize: 2.0,
-                            tooltip: 'Increase volume by 10',
-                            onPressed: () {
-//                              setState(() {
-//                              });
-                              _bloc.confirmChat(item.id, true);
-                            },
-                          ) : Text(''),
-                          FlatButton(
-                            child: Text(
-                              to ? 'CANCEL' : 'CONFIRM',
-                              style: TextStyle(
-                                fontSize: 16.0,
-                                fontFamily: 'Arial',
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                            textColor: Colors.white,
-                            color: Colors.black54,
-                            shape:
-                            RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
-                            onPressed: () {
-                                if(to){
-                                  _bloc.confirmChat(item.id, true);
-                                }
-                                else{
-                                  _bloc.confirmChat(item.id, false);
-                                }
-//                      this.createMessage();
-                              controller.text = '';
-                            },
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                )
+                    )
+                ),
+              ),
             ),
           ),
         ),
@@ -148,90 +206,105 @@ class _ChatItemState extends State<ChatItemScreen> {
 
     return new Container(
       margin: EdgeInsets.only(
-          left: 20.0,
-          right: 20.0,
-          top: 20.0
+        right: 20.0,
+        left: 20.0,
+        top: 20.0
       ),
-      child: new Container(
-        width: MediaQuery.of(context).size.width,
-        height: 40.0,
+      child: Slidable(
+        actionPane: SlidableDrawerActionPane(),
+        actionExtentRatio: 0.25,
+        secondaryActions: <Widget>[
+          IconSlideAction(
+//            caption: 'Delete',
+            icon: Icons.delete,
+            color: Colors.transparent,
+            foregroundColor: HexColor(item.useColorChat(widget._currentUserUid)),
+            onTap: (){
+              _bloc.confirmChat(item.id, true);
+            },
+          ),
+        ],
+        child: new Container(
+          width: MediaQuery.of(context).size.width,
+          height: 40.0,
 //        height: MediaQuery.of(context).size.height,
 
-        child: new Material(
+          child: new Material(
 //          borderRadius: BorderRadius.circular(100.0),
-          child: new Container(
-              decoration: BoxDecoration(
-                color: chatActiveIndex == index ?
-                HexColor(item.useColorChat(widget._currentUserUid)).withOpacity(0.25)
-                    : null,
-                border: Border.all(
-                    width: 2.0,
-                    color: HexColor(item.useColorChat(widget._currentUserUid)).withOpacity(chatActiveIndex == index ? 0.0 : 0.35)
+            child: new Container(
+                decoration: BoxDecoration(
+                  color: chatActiveIndex == index ?
+                  HexColor(item.useColorChat(widget._currentUserUid)).withOpacity(0.25)
+                      : null,
+                  border: Border.all(
+                      width: 2.0,
+                      color: HexColor(item.useColorChat(widget._currentUserUid)).withOpacity(chatActiveIndex == index ? 0.0 : 0.35)
+                  ),
+                  borderRadius: BorderRadius.all(
+                      Radius.circular(150.0) //         <--- border radius here
+                  ),
                 ),
-                borderRadius: BorderRadius.all(
-                    Radius.circular(150.0) //         <--- border radius here
-                ),
-              ),
-              child: new Container(
-                padding: EdgeInsets.all(2.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    new Container(
-                      width: 190.0,
-                      child: TextField(
-                        onTap: (){
-                          setState(() {
-                            chatActiveIndex = index;
-                          });
-                        },
-                        decoration: new InputDecoration(
-                          contentPadding: EdgeInsets.only(left: 10.0),
-                          hintText: "Type a message",
-                          border: InputBorder.none,
-                        ),
-                        onSubmitted: (String str) {
-                          print(str);
+                child: new Container(
+                  padding: EdgeInsets.all(2.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      new Container(
+                        width: 190.0,
+                        child: TextField(
+                          onTap: (){
+                            setState(() {
+                              chatActiveIndex = index;
+                            });
+                          },
+                          decoration: new InputDecoration(
+                            contentPadding: EdgeInsets.only(left: 10.0),
+                            hintText: "Type a message",
+                            border: InputBorder.none,
+                          ),
+                          onSubmitted: (String str) {
+                            print(str);
 //                                text += str;
+                          },
+                          controller: controller,
+                        ),
+                      ),
+                      new Container(
+                          padding: EdgeInsets.only(
+                              right: 10.0
+                          ),
+                          child: Text(
+                            item.userNameChat(widget._currentUserUid),
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          )
+                      ),
+                      FlatButton(
+                        child: Text(
+                          'PUSH',
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            fontFamily: 'Arial',
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        textColor: Colors.white,
+                        color: HexColor(item.useColorChat(widget._currentUserUid)),
+                        shape:
+                        RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+                        onPressed: () {
+                          print(controller.text);
+                          if(controller.text != ''){
+                            _bloc.createMsg(widget._currentUserUid, item.id, controller.text);
+                          }
+                          controller.text = '';
                         },
-                        controller: controller,
                       ),
-                    ),
-                    new Container(
-                      padding: EdgeInsets.only(
-                        right: 10.0
-                      ),
-                      child: Text(
-                        item.userNameChat(widget._currentUserUid),
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      )
-                    ),
-                    FlatButton(
-                      child: Text(
-                        'PUSH',
-                        style: TextStyle(
-                          fontSize: 16.0,
-                          fontFamily: 'Arial',
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      textColor: Colors.white,
-                      color: HexColor(item.useColorChat(widget._currentUserUid)),
-                      shape:
-                      RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
-                      onPressed: () {
-                        print(controller.text);
-                        if(controller.text != ''){
-                          _bloc.createMsg(widget._currentUserUid, item.id, controller.text);
-                        }
-                        controller.text = '';
-                      },
-                    ),
-                  ],
-                ),
-              )
+                    ],
+                  ),
+                )
+            ),
           ),
         ),
       ),
